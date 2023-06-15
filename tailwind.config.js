99322d51e39708d2e89414470625da135f47f076
alapottra-troj1n <alapottratrojon@gmail.com>
2023-06-16 00:01:12 +0600

    done with 2nd section

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,19 @@
 module.exports = {
   content: ["./dist/**/*.{html,js}"],
   theme: {
-    extend: {},
+ 
+
+    extend: {
+      fontFamily: {
+        'poppins': 'Poppins, Arial, sans-serif',
+      },
+      colors: {
+        primary : '#00B4D8',
+        secondary: '#FF6D00',
+        charcoal: '#212121',
+        mygolden: '#FDBE3B'
+      },
+    },
   },
   plugins: [],
 }
